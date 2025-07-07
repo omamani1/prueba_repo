b9c11fbbc6681ee63340efd9d56bf24262ea34fc
OmarRMC <mamani.capcha.omarrodrigo@gmail.com>
2025-07-06 20:14:10 -0400

    prueba

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "📦 Limpiando caché anterior..."
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+
+echo "📌 Ejecutando migraciones..."
+php artisan migrate --force
+
+echo "🚀 Iniciando servidor Laravel en puerto 8080"
+php artisan serve --host=0.0.0.0 --port=8080
